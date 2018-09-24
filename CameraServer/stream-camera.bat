bd7e4e006d28841ec5a2a76e1d185576ab2c689a
Chuck Benedict <chuck.c.benedict@gmail.com>
2018-09-24 08:46:36 -0700

    Make camera parameterizable in startup for windows.

--- CameraServer/stream-camera.bat
+++ CameraServer/stream-camera.bat
@@ -1,1 +1,12 @@
-gst-launch-1.0 ksvideosrc device-index=1 ! image/jpeg, framerate=30/1, width=640, height=480 ! queue ! multipartmux boundary=spionisto ! queue leaky=2 ! tcpclientsink host=127.0.0.1 port=9999
+echo on
+if "%~1"=="" (goto :BLANK) else (goto :NOTBLANK)
+
+:BLANK
+set DEVICEINDEX=0
+goto LAUNCH
+
+:NOTBLANK
+set DEVICEINDEX=%1
+
+:LAUNCH
+gst-launch-1.0 ksvideosrc device-index=%DEVICEINDEX% ! image/jpeg, framerate=30/1, width=640, height=480 ! queue ! multipartmux boundary=spionisto ! queue leaky=2 ! tcpclientsink host=127.0.0.1 port=9999
--- startup.bat
+++ startup.bat
@@ -1,7 +1,13 @@
+REM
+REM The first parameter is the device index of the camera to grab.
+REM Windows assigns them in an unpredictable manner.  A laptop's built in camera
+REM is usually device index 0 but if a USB camera is plugged in at boot, it
+REM might be camera 0.
+REM
 start python ntserver.py
 timeout 3
 start python live-mjpeg-stream.py
 timeout 3
-start stream-camera.bat
+start stream-camera.bat %1
 timeout 3
 start runCameraVision.bat
