d49f1b96955a5620c5ba9705c05cbe7b9a9e2aed
Chuck Benedict <chuck@benedict.email>
2018-10-04 13:18:52 -0700

    Alter startup script to call from pthon venv.

--- scripts/startup.bat
+++ scripts/startup.bat
@@ -1,3 +1,4 @@
+@echo off
 REM
 REM Requires Python 3 and gstream to be installed
 REM
@@ -6,10 +7,12 @@ REM Windows assigns them in an unpredictable manner.  A laptop's built in camera
 REM is usually device index 0 but if a USB camera is plugged in at boot, it
 REM might be camera 0.
 REM
-start ./python/scripts/python ntserver.py
+if exist ./python/Scripts/activate.bat call ./python/Scripts/activate
+start ntserver
 timeout 3
-start ./python/scripts/python live-mjpeg-stream.py
+start ipcamera
 timeout 3
 start stream-camera.bat %1
 timeout 3
 start runCameraVision.bat
+if exist ./python/Scripts/deactivate.bat call ./python/Scripts/deactivate
