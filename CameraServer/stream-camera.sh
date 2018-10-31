f757b1313661739acd9e565f4cec08c32007a9f8
Chuck Benedict <chuck@benedict.email>
2018-10-31 11:54:56 -0700

    Mod resolution to get acceptable performance from Pi.

--- CameraServer/stream-camera.sh
+++ CameraServer/stream-camera.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gst-launch-1.0 v4l2src device=/dev/video${1:-0} ! image/jpeg, framerate=30/1, width=640, height=480 ! queue ! multipartmux boundary=spionisto ! queue leaky=2 ! tcpclientsink host=127.0.0.1 port=9999
+gst-launch-1.0 v4l2src device=/dev/video${1:-0} ! image/jpeg, framerate=30/1, width=320, height=240 ! queue ! multipartmux boundary=spionisto ! queue leaky=2 ! tcpclientsink host=127.0.0.1 port=9999
--- CameraServer/stream-test.sh
+++ CameraServer/stream-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gst-launch-1.0 videotestsrc pattern=ball ! video/x-raw, framerate=15/1, width=640, height=480 !  jpegenc ! multipartmux boundary=spionisto ! tcpclientsink host=127.0.0.1 port=9999
+gst-launch-1.0 videotestsrc pattern=ball ! video/x-raw, framerate=15/1, width=320, height=240 !  jpegenc ! multipartmux boundary=spionisto ! tcpclientsink host=127.0.0.1 port=9999
 
